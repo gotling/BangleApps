01868204c921e26c358c72bc84325a25e4476d8d
Marcus Gotling <marcus@gotling.se>
2024-08-13 19:38:32 +0200

    First

---
+++ apps/gosyrun/README.md
@@ -0,0 +1,10 @@
+# App Name
+
+An app for run training.
+
+![](screenshot.png)
+
+
+## Creator
+
+Marcus Gotling
---
+++ apps/gosyrun/lib.js
@@ -0,0 +1,5 @@
+const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+exports.getWeekDay = function(day) {
+    return weekdays[day];
+}
